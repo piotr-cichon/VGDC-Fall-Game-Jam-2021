32fc2f1b1a35acc62ba8f895362829be5bbecdcc
Nicolae Filat <nicolae.filat@gmail.com>
2021-11-21 20:37:28 +0100

    Fixed canvas bugs and added Pause Menu

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private KeyCode pauseMenuKey = KeyCode.Q;
     [SerializeField] private GameObject doubleJumpImage;
     [SerializeField] private GameObject teleportShootImage;
+    [SerializeField] private GameObject pauseMenu;
 
+    private Player _player;
     void Awake()
     {
+        _player = FindObjectOfType<Player>();
+        pauseMenu.SetActive(false);
         doubleJumpImage.SetActive(false);
         teleportShootImage.SetActive(false);
     }
@@ -32,5 +38,38 @@ public class UIManager : MonoBehaviour
     {
         DoubleJumpPowerup.onDoubleJumpPickup -= ActivateDoubleJump;
         PortalPickup.onPortalPickup -= ActivateTeleport;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(pauseMenuKey))
+        {
+            PauseMenu();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Escape) && pauseMenu.activeSelf)
+        {
+            ResumeMenu();
+        }
+    }
+
+    public void ResumeMenu()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void PauseMenu()
+    {
+        
+        pauseMenu.SetActive(true);
     }
 }
